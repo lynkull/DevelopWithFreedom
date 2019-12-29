9c773f5c722c5481bf83741a89c59cb95c8aadbd
Ed Cres <crespoed07@gmail.com>
2019-12-28 21:27:22 -0500

    new repo

---
+++ app/src/main/java/com/aldreduser/developwithfreedom/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.aldreduser.developwithfreedom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// use this app to build anything adn test your skills
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
