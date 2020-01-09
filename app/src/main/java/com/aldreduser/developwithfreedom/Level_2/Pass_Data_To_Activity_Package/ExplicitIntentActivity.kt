dcde63b34a80f3bba248c7b53e351e800d88bc85
Ed Cres <crespoed07@gmail.com>
2020-01-09 00:00:16 -0500

    created new activity

---
+++ app/src/main/java/com/aldreduser/developwithfreedom/Level_2/Pass_Data_To_Activity_Package/ExplicitIntentActivity.kt
@@ -0,0 +1,12 @@
+package com.aldreduser.developwithfreedom.Level_2.Pass_Data_To_Activity_Package
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExplicitIntentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_explicit_intent)
+    }
+}
