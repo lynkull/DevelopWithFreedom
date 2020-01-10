0eb03cf245cf21b783aa442572f3435bbba5892c
Ed Cres <crespoed07@gmail.com>
2020-01-09 22:50:47 -0500

    passing a list to an activity

--- app/src/main/java/com/aldreduser/developwithfreedom/Level_2/PassDataToActivityActivity.kt
+++ app/src/main/java/com/aldreduser/developwithfreedom/Level_2/PassDataToActivityActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.aldreduser.developwithfreedom.Level_2.Pass_Data_To_Activity_Package.ArrayBeingPassedActivity
 import com.aldreduser.developwithfreedom.Level_2.Pass_Data_To_Activity_Package.TestActivityOne
 import com.aldreduser.developwithfreedom.R
 
@@ -18,6 +19,24 @@ class PassDataToActivityActivity : AppCompatActivity() {
         var intent = Intent(this, TestActivityOne::class.java)
 
         intent.putExtra("thisnamething", "COURSETITLEOBJECTTOPASS")
+
+        startActivity(intent)
+    }
+
+    // some objects cannot be attached to bundles (like .putExtra)
+    // some objects should not be attached to bundles (Very large data sets, like a large array list (50 entries))
+
+    // parcelable, Serializable
+    // passing classes to an activity.
+    // maybe this is not recommended. Only pass data instead.
+
+    fun passArrayListClicked(view: View) {
+        var numbers = listOf<Int>(56, 2,3 ,8 ,9)
+
+        var intent = Intent(this, ArrayBeingPassedActivity::class.java)
+
+        // to pass a list, need to use parcealable or maybe serializable
+        // intent.putExtra("listOfNumbers", numbers)
 
         startActivity(intent)
     }
--- app/src/main/java/com/aldreduser/developwithfreedom/Level_2/Pass_Data_To_Activity_Package/ArrayBeingPassedActivity.kt
+++ app/src/main/java/com/aldreduser/developwithfreedom/Level_2/Pass_Data_To_Activity_Package/ArrayBeingPassedActivity.kt
@@ -2,11 +2,12 @@ package com.aldreduser.developwithfreedom.Level_2.Pass_Data_To_Activity_Package
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.aldreduser.developwithfreedom.R
 
-class ExplicitIntentActivity : AppCompatActivity() {
+class ArrayBeingPassedActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_explicit_intent)
+        setContentView(R.layout.activity_array_being_passed)
     }
 }
