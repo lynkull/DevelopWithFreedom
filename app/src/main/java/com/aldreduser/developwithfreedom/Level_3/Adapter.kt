998c3472234d6c7c017825cd3562d59478ce4bed
Ed Cres <crespoed07@gmail.com>
2020-02-14 23:59:11 -0500

    better implementation of recyclerview

---
+++ app/src/main/java/com/aldreduser/developwithfreedom/Level_3/Adapter.kt
@@ -0,0 +1,34 @@
+package com.aldreduser.developwithfreedom.Level_3
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.aldreduser.developwithfreedom.R
+import kotlinx.android.synthetic.main.level3_recyclerview_item.view.*
+
+class Adapter(val nContext:Context, val textx:ArrayList<String>, val images:ArrayList<Int>): RecyclerView.Adapter<Adapter.viewHolder>() {
+
+    //this method is returning the view for each item in the list
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): viewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val callForRow = layoutInflater.inflate(R.layout.level3_recyclerview_item, parent, false)
+        return viewHolder(callForRow)
+    }
+
+    //this method is binding the data on the list
+    override fun getItemCount(): Int {
+        return textx.size
+    }
+
+    //this method is giving the size of the list
+    override fun onBindViewHolder(holder: viewHolder, position: Int) {
+        holder.itemView.texts.text = textx.get(position)
+        holder.itemView.images.setImageResource(images.get(position))
+    }
+
+    public class viewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
+}
--- app/src/main/java/com/aldreduser/developwithfreedom/Level_3/RecyclerViewActivity.kt
+++ app/src/main/java/com/aldreduser/developwithfreedom/Level_3/RecyclerViewActivity.kt
@@ -4,19 +4,45 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.aldreduser.developwithfreedom.R
+import kotlinx.android.synthetic.main.level3_activity_recycler_view.*
 
 // made a drawable xml, and level3_recyclerview_item1.xml
 
 class RecyclerViewActivity : AppCompatActivity() {
+    var textx:ArrayList<String> = ArrayList()
+    var images:ArrayList<Int> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.level3_activity_recycler_view)
+
+        loadData()
+        val adapter = Adapter(this, textx, images)
+
+        recycler.layoutManager = LinearLayoutManager(this)
+        recycler.adapter = adapter
     }
 
-    fun recycler2ButtonClicked(view: View) {
-        val intent = Intent(this, BetterRecyclerView2::class.java)
-        startActivity(intent)
+    private fun loadData() {
+        textx.add("Text 1")
+        images.add(R.drawable.ic_launcher_background)
+        textx.add("Text 2")
+        images.add(R.drawable.ic_launcher_background)
+        textx.add("Text 3")
+        images.add(R.drawable.ic_launcher_background)
+        textx.add("Text 4")
+        images.add(R.drawable.ic_launcher_background)
+        textx.add("Text 5")
+        images.add(R.drawable.ic_launcher_background)
+        textx.add("Text 6")
+        images.add(R.drawable.ic_launcher_background)
+        textx.add("Text 7")
+        images.add(R.drawable.ic_launcher_background)
+        textx.add("Text 8")
+        images.add(R.drawable.ic_launcher_background)
     }
+
+
 }
