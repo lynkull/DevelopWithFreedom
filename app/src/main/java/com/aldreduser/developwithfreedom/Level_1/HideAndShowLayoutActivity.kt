87a1d05dd016bda4864abd3e66ddde125368bce5
Ed Cres <crespoed07@gmail.com>
2020-05-04 23:59:42 -0400

    implementing custom toolbar

--- app/src/main/java/com/aldreduser/developwithfreedom/Level_1/HideAndShowLayoutActivity.kt
+++ app/src/main/java/com/aldreduser/developwithfreedom/Level_1/HideAndShowLayoutActivity.kt
@@ -3,7 +3,10 @@ package com.aldreduser.developwithfreedom.Level_1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import com.aldreduser.developwithfreedom.R
@@ -32,6 +35,7 @@ class HideAndShowLayoutActivity : AppCompatActivity() {
         }
     }
 
+    //hide and show a layout
     fun toggleTextButtonPressed(view: View) {
         val hiddenText = hidden_text        // try to make this private
         if(hiddenText.isVisible){
@@ -41,7 +45,36 @@ class HideAndShowLayoutActivity : AppCompatActivity() {
         }
     }
 
+    //changing the look of the actionbar (text size, color, etc)
     fun actionBarButtonPressed(view: View) {
+        // Changing the action bar can be done in manifest file, programmatically in file, or in resource file
+        //https://www.youtube.com/watch?v=510WE8CmiXI       by changing it in resource files (quick fix)
+        //https://www.youtube.com/watch?v=DMkzIOLppf4       by creating a toolbar xml file (BEST PRACTICE, more customizable)
+        //      if doing this, need to change to "Theme.AppCompat.Light.NoActionBar" in styles.xml file
 
+        //this is programatically
+        title = "Whatever title you want."
+        //font size
+        //gravity (center)
+        //      these crash  my app:
+        //back button   actionBar.setHomeButtonEnabled(true); // actionBar.setDisplayHomeAsUpEnabled(true)
+        //actionBar.setIcon(R.drawable.ic_android) //this didn't work but idek what an icon is used for here
+    }
+
+    //Options Menu button. In action bar (...)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.level1_layout_options_menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val id = item?.itemId
+        if(id == R.id.add_action){
+            Toast.makeText(this, "item Add clicked", Toast.LENGTH_SHORT).show()
+            return true
+        }else{ //if (id == R.id.settings_action)
+            Toast.makeText(this, "Setting clicked", Toast.LENGTH_SHORT).show()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
