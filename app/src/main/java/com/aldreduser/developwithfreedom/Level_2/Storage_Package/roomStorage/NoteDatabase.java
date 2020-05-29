99c4e8b7cd7183e8f569e490c0c5a1775e156802
Ed Cres <crespoed07@gmail.com>
2020-05-28 23:14:51 -0400

    the database table is populated on creation

--- app/src/main/java/com/aldreduser/developwithfreedom/Level_2/Storage_Package/roomStorage/NoteDatabase.java
+++ app/src/main/java/com/aldreduser/developwithfreedom/Level_2/Storage_Package/roomStorage/NoteDatabase.java
@@ -1,10 +1,13 @@
 package com.aldreduser.developwithfreedom.Level_2.Storage_Package.roomStorage;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = {Note.class}, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
@@ -18,8 +21,34 @@ public abstract class NoteDatabase extends RoomDatabase {
         if (instance == null) {
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     NoteDatabase.class, "note_database")
-                    .fallbackToDestructiveMigration().build();
+                    .fallbackToDestructiveMigration()
+                    .addCallback(roomCallBack)  //when the instance is first created, it will execute super.onCreate(db) and populate the database
+                    .build();
         }
         return instance;
+    }
+
+    // populate the database table on creation
+    private static RoomDatabase.Callback roomCallBack = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
+        private NoteDao noteDao;
+
+        private PopulateDbAsyncTask(NoteDatabase db) {
+            noteDao = db.noteDao(); // this ia possible bc onCreate is called after the database was created
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("Title 1", "Description 1", 1));
+            noteDao.insert(new Note("Title 2", "Description 2", 2));
+            noteDao.insert(new Note("Title 3", "Description 3", 3));
+            return null;
+        }
     }
 }
