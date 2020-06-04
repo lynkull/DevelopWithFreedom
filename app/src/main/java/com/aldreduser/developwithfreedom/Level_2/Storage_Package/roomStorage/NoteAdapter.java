d63ccd504ef2185d9cd47601eff769adca0fe547
Ed Cres <crespoed07@gmail.com>
2020-06-04 03:51:31 -0400

    creating recyclerview of the database

---
+++ app/src/main/java/com/aldreduser/developwithfreedom/Level_2/Storage_Package/roomStorage/NoteAdapter.java
@@ -0,0 +1,54 @@
+package com.aldreduser.developwithfreedom.Level_2.Storage_Package.roomStorage;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.aldreduser.developwithfreedom.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
+    private List<Note> notes = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //get context
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.level2_note_item, parent, false);
+        return new NoteHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder holder, int position) {
+        //get data from the single Note java object into the views of the NoteHolder
+        Note currentNote = notes.get(position);
+        holder.textViewTitle.setText(currentNote.getTitle());
+        holder.textViewDescription.setText(currentNote.getDescription());
+        holder.textViewPriority.setText(String.valueOf(currentNote.getPriority())); //turns integer to a string
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    class NoteHolder extends RecyclerView.ViewHolder {
+        private TextView textViewTitle;
+        private TextView textViewDescription;
+        private TextView textViewPriority;
+
+        public NoteHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewTitle = itemView.findViewById(R.id.text_view_title);
+            textViewDescription = itemView.findViewById(R.id.text_view_title);
+            textViewPriority = itemView.findViewById(R.id.text_view_priority);
+        }
+    }
+}
